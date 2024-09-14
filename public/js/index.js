398e08979cc08b964b915d4fb1ad8cebf120ff7f
Eren İşci <isci.eren@gmail.com>
2024-09-14 20:13:05 +0300

    public

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+import '@babel/polyfill';
+import { login, logout } from './login';
+
+// DOM Elements
+const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
+
+if (loginForm)
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
